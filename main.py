2532058622f102083fd75c7b305b7e35cb4af089
lightpurple9273 <geog35270@gmail.com>
2025-02-19 13:12:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,15 @@
 
 import gpcdc
 
-#import pandas as pd
-#import requests
 import datetime
 import os
 
 tables = gpcdc.Tables()
 anun = gpcdc.Announcement()
 
-anun.create_message(datetime.date.today(),tables)
+#anun.create_message(datetime.date.today(),tables)
 #print(anun.message)
-anun.post(os.getenv("WEBHOOK"))
+#anun.post(os.getenv("WEBHOOK"))
     
 anun.create_message(datetime.date.today()+datetime.timedelta(days=1),tables)
 #print(anun.message)
