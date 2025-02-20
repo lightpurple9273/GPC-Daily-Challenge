069afadce4ba262a37a6a18d862738fa155a9d1e
lightpurple9273 <geog35270@gmail.com>
2025-02-20 16:05:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ anun = gpcdc.Announcement()
 #print(anun.message)
 #anun.post(os.getenv("WEBHOOK"))
     
-anun.create_message(datetime.date.today()+datetime.timedelta(days=1),tables)
+anun.create_message(datetime.date.today()+datetime.timedelta(days=1),tables,supress_ping=bool(True))
 #print(anun.message)
 anun.post(os.getenv("WEBHOOK"))
