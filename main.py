6ead7410a049a625898a538ad7c274dcb7ee456d
lightpurple9273 <geog35270@gmail.com>
2025-02-20 17:32:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ anun = gpcdc.Announcement()
 anun.create_message(datetime.date.today(),tables)
 anun.post(os.getenv("WEBHOOK_PUBLICATION"))
     
-anun.create_message(datetime.date.today()+datetime.timedelta(days=1),tables,supress_ping=bool(True))
+anun.create_message(datetime.date.today()+datetime.timedelta(days=1),tables,
+                    supress_ping=bool(True),
+                   pre_message="Tomorrow's DC announcement will be...\n====\m",
+                   post_message="\n====\nA ping will be added for tomorrow.")
 anun.post(os.getenv("WEBHOOK_DRAFT"))
 
 response = requests.post(os.getenv("WEBHOOK_LOG"),json={"content":"DC posting successful for today!",})
