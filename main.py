10b991cd79c11c00abe16142adca6f4db2d2127a
lightpurple9273 <geog35270@gmail.com>
2025-02-22 20:00:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ tables = gpcdc.Tables()
 anun = gpcdc.Announcement()
 
 anun.create_message(datetime.date.today(),tables)
-anun.post(os.getenv("WEBHOOK_PUBLICATION"))
+anun.post(os.environ["WEBHOOK_PUBLICATION"])
     
 anun.create_message(datetime.date.today()+datetime.timedelta(days=1),tables,
                     suppress_ping=bool(True),
                    pre_message="Tomorrow's DC announcement will be...\n====\n",
                    post_message="\n====\nA ping for `@GPC Daily Challenge`, role id 1172389612665180190, will be added for tomorrow.")
-anun.post(os.getenv("WEBHOOK_DRAFT"))
+anun.post(os.environ["WEBHOOK_DRAFT"])
 
-if not (requests.post(os.getenv("WEBHOOK_LOG"),json={"content":"DC posting successful for today!",})).status_code == 204:
+if not (requests.post(os.environ["WEBHOOK_LOG"],json={"content":"DC posting successful for today!",})).status_code == 204:
     raise Exception(f"Failed to send message: {response.status_code}")
-    
+
+try:
+    for i in range(10):
+        anun.create_message(datetime.date.today()+datetime.timedelta(days=i),tables)
+        
