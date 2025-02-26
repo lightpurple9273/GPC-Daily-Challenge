297f3c0baa95aedcd7257fda9316760e29e6ca2d
lightpurple9273 <geog35270@gmail.com>
2025-02-26 17:32:38 +0900

    Update gpcdc.py

--- gpcdc.py
+++ gpcdc.py
@@ -15,13 +15,6 @@ class Tables:
             self.dfs[key] = pd.read_html(f'{os.environ["LABEL"]}{os.environ[key]}')[0]
         return self.dfs[key]
 
-#    def mass_read(self):
-#        for year in range(2013,int(datetime.date.today().year)+int(11)):
-#            for month in range(1,13):
-#                table_of_the_month = f"TABLE{year}{month:02}"
-#                if os.environ[table_of_the_month] is not None:
-#                    self.dfs[f"{year}{month:02}"] = pd.read_html(f'{os.environ["LABEL"]}{os.environ[table]}')[0]
-
 class Announcement:
     def __init__(self):
         pass
