9e3f20949b96ca3957858c9d08435ee536a2de63
lightpurple9273 <geog35270@gmail.com>
2025-03-03 09:23:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ conf = df.iloc[is_today, 2:6].values[0]
 message = f"__**GPC Daily Challenge #{challenge_number}**__\nMap: **{conf[0]}**\nSettings: {conf[1]}, {conf[2]}\n<{conf[3]}>\n<@&1172389612665180190>"
 json_message={"content":message,}
 
-response = requests.post(os.getenv("WEBHOOK"),json=json_message)
-
-if response.status_code == 204:
-    print("Message sent successfully!")
-else:
-    print(f"Failed to send message: {response.status_code}")
+#response = requests.post(os.getenv("WEBHOOK"),json=json_message)
+#
+#if response.status_code == 204:
+#    print("Message sent successfully!")
+#else:
+#    print(f"Failed to send message: {response.status_code}")
